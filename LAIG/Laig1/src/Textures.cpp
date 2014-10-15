13440f4ce657523eee1deb13df40403fa367c03a
Psouka <p4so@gmail.com>
2014-10-15 19:01:05 +0100

    fail

--- LAIG/Laig1/src/Textures.cpp
+++ LAIG/Laig1/src/Textures.cpp
@@ -5,7 +5,7 @@ Texture :: Texture(string i, string f, float tex_s,float tex_t){
 id = i;
 file = f;
 texlength_s = tex_s;
-texlength_t = texlength_t;
+texlength_t = tex_t;
 CGFtextura = new CGFtexture(file);
 }
 
