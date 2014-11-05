3fd5e9be08bb8ba563c8fee64f511abed7790e37
Psouka <p4so@gmail.com>
2014-11-05 10:49:00 +0000

    oh baby, a triple

--- PLOG/src/p.pl
+++ PLOG/src/p.pl
@@ -1,8 +1,6 @@
 :-use_module(library(random)).
-:-use_module(library(lists)).
 board(        
-[
- [0,' ',0,' ',0,' ',0,' ',0,' ',0,' ',0],
+[[0,' ',0,' ',0,' ',0,' ',0,' ',0,' ',0],
  [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
  [0,' ',0,' ',0,' ',0,' ',0,' ',0,' ',0],
  [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
@@ -14,8 +12,7 @@ board(
  [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
  [0,' ',0,' ',0,' ',0,' ',0,' ',0,' ',0],
  [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
- [0,' ',0,' ',0,' ',0,' ',0,' ',0,' ',0]
-]
+ [0,' ',0,' ',0,' ',0,' ',0,' ',0,' ',0]]
 ).
 
 
@@ -23,9 +20,6 @@ board(
 % 0 para celulas neutras
 % A para células com peão do jogador 1 sem parede por baixo
 % B para células com peão do jogador 2 sem parede por baixo
-
-
-
 
 %----------------------------------------------------------------------------
 % check mov limit
@@ -38,6 +32,14 @@ checkLimit(Xinicial, Yinicial, Xfinal, Yfinal):-
         Yinicial < 7,
         Yfinal > -1,
         Yfinal < 7.
+
+checkLimit(X,Y):-
+       X > -1,
+        X < 13,
+       Y> -1,
+       Y< 13.
+
+
 
 %----------------------------------------------------------------------------
 %print lists
@@ -59,8 +61,8 @@ drawBoard(B):- nl,printline(['   ',0,' ',1,' ',2,' ',3,' ',4,' ',5, ' ',6]), nl,
 
 
 %----------------------------------------------------------------------------
-% 
-elementAt([], _, -1):-!, fail.
+% Returns Element as the element of the list of lists at [Yindex][Xindex]     
+elementAt([], _, -1):-!,fail.
 
 elementAt([Elem|_], Elem, 0).
 
@@ -73,10 +75,21 @@ elementAt([], _, _, -1):-!, fail.
 elementAt([H|_], Elem, X, 0):-
         elementAt(H, Elem, X).
 
+%X = Col, Y = Line
 elementAt([_|T], Elem, X, Y):-
           elementAt(T, Elem, X, NY),
           Y is NY+1.
 
+
+%----------------------------------------------------------------------------
+% Change player turn
+
+changeActivePlayer(P, P1) :-
+    P = 1,
+    P1 is 2.
+changeActivePlayer(P, P1) :-
+    P = 2,
+    P1 is 1.
 
 %----------------------------------------------------------------------------
 % Writes Turn
@@ -87,59 +100,90 @@ print_turn(2):-
         write('Red turn: '),
         nl.
 
-
 %----------------------------------------------------------------------------
 % Replaces the value of the element in the index Index to Elem 
-replace( [L|Ls] , 0 , Y , Z , [R|Ls] ) :- 
-  replace_column(L,Y,Z,R)                
-  .                                       
-replace( [L|Ls] , X , Y , Z , [L|Rs] ) :- 
-  X > 0 ,                                
-  X1 is X-1 ,                            
-  replace( Ls , X1 , Y , Z , Rs )         
-  .                                       
+replace([_|T], 0, Elem, [Elem|T]).
 
-replace_column( [_|Cs] , 0 , Z , [Z|Cs] ) . 
-replace_column( [C|Cs] , Y , Z , [C|Rs] ) :-
-  Y > 0 ,                                    
-  Y1 is Y-1 ,                              
-  replace_column( Cs , Y1 , Z , Rs )       
-  .                                          
+replace([H|T], Index, Elem, [H|R]):- 
+        Index > -1, 
+        NIndex is Index-1, 
+        replace(T, NIndex, Elem, R), 
+        !.
 
+replace(List, _, _, List).
+
+
+% Replaces the value of the element in (IndexX, IndexY) to Elem
+replace([H|T], IndexX, 0, Elem, [X|T]):-
+        replace(H, IndexX, Elem, X).
+                                          
+replace([H|T], IndexX, IndexY, Elem, [H|R]):-
+        IndexY > -1, 
+        NIndexY is IndexY-1, 
+        replace(T, IndexX, NIndexY, Elem, R), 
+        !.
+        
+replace(List, _, _,_, List).            
+
+%-------------------------------------------------------------------
+%make move
+makeMov(B,Xi,Yi,Xf,Yf).
 
 
 %---------------------------------------------------------------
 %check move
 
 checkCurve(B,Xinicial, Yinicial, Xfinal, Yfinal).
-checkMov(B,Xinicial, Yinicial, Xfinal, Yfinal) :-checkLimit(Xinicial, Yinicial, Xfinal, Yfinal),!, checkCurve(Xinicial, Yinicial, Xfinal, Yfinal).
-checkMov(B,Xinicial, Yinicial, Xfinal, Yfinal) :- fail.
+checkMov(B,Xinicial, Yinicial, Xfinal, Yfinal) :-checkLimit(Xinicial, Yinicial, Xfinal, Yfinal),!, checkCurve(B,Xinicial, Yinicial, Xfinal, Yfinal).
+checkMov(_,_, _, _, _) :- fail.
 
-checkMov(B,X,Y).
+
+%colocar um peao
+checkMov(B,X,Y) :- checkLimit(X,Y),elementAt(B, Elem, X, Y), Elem \= ' ', !, checkArea(NrP,X,Y,B,_), NrP > 1,!.
+checkMov(_,_,_) :-fail.
+
+
+
+addNrP('A',NrP) :- NrP is 1.
+addNrP('B',NrP) :- NrP is 1.
+addNrP(_,NrP) :- NrP is 0.
+
+checkArea(NrP,X,Y,ToVisit,Visited):- checkLimit(X,Y),elementAt(ToVisit,Elem,X,Y), Elem \= 'X',!,addNrP(Elem,NrI),
+        replace(ToVisit,X,Y,'X',V0),
+        Nx is X+1,
+        checkArea(NrP1,Nx,Y,V0,V1),
+         Ny is Y +1,
+       checkArea(NrP3,X,Ny,V1,V2),
+        Nx2 is X-1,
+       checkArea(NrP2,Nx2,Y,V2,V3),
+        Ny2 is Y-1,
+        checkArea(NrP4,X,Ny2,V3,V4),
+        Visited = V4,
+        NrP is NrI + NrP1 + NrP2 + NrP3 + NrP4
+        .  
+
+
+checkArea(0, _, _, T, T).
 %-----------------------------------------------------------------------
 %input para a jogada
 getMov(B,Xi,Yi,Xf,Yf) :- nl,
-                        print('\nCoordenada X da peca a mover : '),read(Yi),
-                        print('\nCoordenada Y da peca a mover : '),read(Xi),
-                        print('\nCoordenada X da casa destino : '),read(Yf),
-                        print('\nCoordenada Y da casa destino : '),read(Xf), checkMov(B,Xi,Yi,Xf,Yf), ! .
-
+                        print('\nColuna da peca a mover : '),read(Xi),
+                        print('\nLinha da peca a mover : '),read(Yi),
+                        print('\nColuna da casa destino : '),read(Xf),
+                        print('\nLinha da casa destino : '),read(Yf), checkMov(B,Xi,Yi,Xf,Yf), ! .
 getMov(Xi,Yi,Xf,Yf) :- nl,write('Jogada nao permitida'),getMov(Xi,Yi,Xf,Yf).
 
 getPiece(B,X,Y):- nl,
-                        print('\nCoordenada X da peca: '),read(Y),
-                        print('\nCoordenada Y da peca: '),read(X),checkMov(B,X,Y), ! .
+                        print('\nColuna da peca: '),read(X),
+                        print('\nLinha da peca: '),read(Y),checkMov(B,X,Y), ! .
 getPiece(B,X,Y) :- nl,write('Posicao nao permitida'),getPiece(B,X,Y).
 
 mov(B,1) :- getPiece(B,_,_).
 mov(B,2) :- getMov(B,_,_,_,_).
 mov(B,_) :- selectMov(B,_).
-   
+    
 
 selectMov(B,R) :- print('\n[1]Colocar peao\n[2]Mover peao\n'),read(R), mov(B,R).
-%-------------------------------------------------------------------
-%make move
-makeMov(Xi,Yi,Xf,Yf) .
 
 
 %-----------------------------------------------------------------------
@@ -151,19 +195,35 @@ getIntro(P) :- write('\nTurno do jogador: '), write(P), nl.
 randomPlayer(P) :- random(1, 3,NrP), NrP == 1,!, P = 'A'. 
 randomPlayer(P) :- P = 'B'.
 
-%------------------------------------------------------------------------
-%check if is a closed area
-
-checkCLosedArea(B,X,Y):- nrP = 0.
-
-
-
 %-----------------------------------------------------------------------
 %Start
+play :-  board(B), drawBoard(B).
+
+% ---------------------------------MAIN MENU------------------------------------
+fines:-
+	mainMenu,
+	read(X),
+	mainMenuOption(X).
+
+mainMenu:-
+	nl,nl,
+	write('----------------------------------------'),nl,
+	write('Fines Game'),nl,nl,
+	write('1- Play'), nl,
+	write('2- Exit'), nl.
+
+mainMenuOption(X):-
+	(
+		X = 1 -> play;
+		X = 2 -> write('Goodbye!');
+		(write('Wrong command!'),nl,fines)
+	).
+
 play(B,P) :- getIntro(P), drawBoard(B), selectMov(B,_), P =='A',!,play(B,'B').
 play(B,P) :- P = 'A',play(B,P).
 start:-  board(B), randomPlayer(P),play(B,P).
 
 
-
-test:- board(B),replace(B, 12, 12,1,NewB),drawBoard(NewB) .
+test:- board(B),checkArea(NrP,0,0,B,C), drawBoard(C) ,write(NrP).
+test2:- board(B),elementAt(B,Elm,12,12),write(Elm). %ElementAt X,Y
+test3:- board(B),replace(B,0,6,'C',C),drawBoard(C). % Replace Y,X
