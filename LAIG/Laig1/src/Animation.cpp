523cee60dd9b6ff8f6d79bc6cb20ef4932ba7b05
Vítor Teixeira <vitor_filipe_@hotmail.com>
2014-11-17 22:43:07 +0000

    dadas

--- LAIG/Laig1/src/Animation.cpp
+++ LAIG/Laig1/src/Animation.cpp
@@ -11,19 +11,30 @@ string Animation:: getId(){
 
 LinearAnimation::LinearAnimation(string id, float span, vector<vector<float>> controlPoint)
 	:Animation(id,span),controlPoint(controlPoint){
-		float tempX, tempY, tempZ;
 
+}
+
+void LinearAnimation::update(unsigned long t) {
+
+}
+
+void LinearAnimation::init(unsigned long t) {
 
 }
 
 CircularAnimation :: CircularAnimation(string id, float span, float* center, float radius, float startAng, float rotAng)
 	:Animation(id,span),radius(radius),startAng(startAng),rotAng(rotAng) {
 
-	this->center[0] = center[0];
-	this->center[1] = center[1];
-	this->center[2] = center[2];
+		this->center[0] = center[0];
+		this->center[1] = center[1];
+		this->center[2] = center[2];
 }
 
-void LinearAnimation::update(unsigned long t) {
+void CircularAnimation::update(unsigned long t) {
 
 }
+
+void CircularAnimation::init(unsigned long t) {
+
+}
+
