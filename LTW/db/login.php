7ce08e7a07f61b0dc06f7bb916a39a46a4aa5f19
PSousa <p4sousa@gmail.com>
2014-11-23 19:48:28 +0000

    nhe

---
+++ LTW/db/login.php
@@ -0,0 +1,39 @@
+<?php
+
+	$dbh = new PDO('sqlite:database.db');
+
+	$username = $_POST['usernameL'];
+	$password = $_POST['passwordL'];
+
+	$check = 0;
+	$stmt = $dbh->prepare('SELECT username, password FROM User WHERE username = ? AND password = ?');
+	$stmt->execute(array($username, $password));
+
+	while ($row = $stmt->fetch()) {
+		//print_r($row);
+ 		if (in_array($username, $row)) {
+ 			if ($password === $row['password']) {
+ 				$check = 1;
+ 				printf ("Welcome back, %s!", $row['username']);
+ 				break;
+ 			}
+ 			//else echo("asd");
+ 		}
+ 		//echo("asd");
+	}
+	if($check == 0)
+		echo('Sorry bro, wrong username or password :(');
+
+?>
+
+<html lang="en">
+  <head>
+    <title>Back to the form</title>
+    <meta charset="utf-8">
+  </head>
+
+  <body>
+  	<br>
+ 	<a href="page.html"> ---Back--- </a>
+  </body>
+</html>
---
+++ LTW/db/register.php
@@ -0,0 +1,42 @@
+<?php
+	$dbh = new PDO('sqlite:database.db');
+	$username = $_POST['usernameR'];
+	$email = $_POST['emailR'];
+	$password = $_POST['passwordR'];
+	$check = 0;
+	$stmt1 = $dbh->prepare('SELECT username FROM User WHERE username = ?');
+	$stmt1->execute(array($username));
+	$result = $stmt1->fetchAll();
+	foreach ($result as $row) {
+
+ 		if (in_array($username, $row)) {
+ 			$check = 1;
+ 			echo "That username has already been taken <br>
+ 				  Please, choose another one";
+ 			break;
+ 		}	
+ 			
+	}
+	if($check == 0) {
+		$idAccount=0;
+		$id1 = $dbh->prepare("SELECT count(*) FROM account");
+		$id1->execute();
+		$id2 = $id1->fetch();
+		//echo $id3[0];
+		$stmt2 = $dbh->prepare('INSERT INTO account (idAccount, username, email, password) VALUES (?, ?, ?, ?)');
+		$stmt2->execute(array($id2[0], $username, $email, $password));
+		printf ("YAY, Welcome to the Big Dick Club %s!", $username);
+	}
+?>
+
+<html lang="en">
+  <head>
+    <title>Back to the form</title>
+    <meta charset="utf-8">
+  </head>
+
+  <body>
+  	<br>
+ 	<a href="page.html"> ---Back--- </a>
+  </body>
+</html>
