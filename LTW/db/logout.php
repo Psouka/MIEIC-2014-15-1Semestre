32489d801c4d775d95546e30bab14b840da0de59
PSousa <p4sousa@gmail.com>
2014-11-25 10:27:16 +0000

    logout fixed, tested

--- LTW/db/logout.php
+++ LTW/db/logout.php
@@ -1,32 +1,20 @@
 <?php
 session_start(); 
 	$dbh = new PDO('sqlite:database.db');
-
-
 	$username = $_SESSION['usernameOn'];
 	$ip = $_SESSION['ipOut'];
+
 if($username != 'test'){
-	$check = 0;
-	$stmt = $dbh->prepare('SELECT username, IPUser FROM UserLogin WHERE username = ?');
-	$stmt->execute(array($username, $password));
 
-	while ($row = $stmt->fetch()) {
-		//print_r($row);
- 		if (in_array($username, $row)) {
- 			if ($username === $row['username'] and $ip === $row['IPUser']) {
- 				$check = 1;
- 				printf ("Good Bye, %s!", $row['username']);
 
- 				$stmt1 = $dbh->prepare('DELETE FROM UserLogin WHERE username = ? and IPUser = ?');
- 				$stmt1->execute(array($username,$ip));
- 				break;
- 			}
- 			//else echo("asd");
- 		}
- 		//echo("asd");
-	}
-	if($check == 0)
-		echo('Something wrong :(');
+	$stmt = $dbh->prepare('SELECT idUser FROM User WHERE username = ?');
+	$stmt->execute(array($username));
+	$row = $stmt->fetch();
+	
+
+	$stmt1 = $dbh->prepare('DELETE FROM UserLogin WHERE idUser = ? and IPUser = ?');
+ 	$stmt1->execute(array($row['idUser'],$ip));
+ 	
 }
 header( 'Location: ../page.html' ) ;
 exit();
