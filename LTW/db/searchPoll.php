4ab53f9ecba4a223b3e6b181ff75bb6cbc2f118e
Psouka <p4so@gmail.com>
2014-11-26 23:07:08 +0000

    php op

--- LTW/db/searchPoll.php
+++ LTW/db/searchPoll.php
@@ -17,27 +17,29 @@ if(isset($_GET['username'])){
   $stmt = $db->prepare('SELECT Question FROM UserQuery WHERE idUser = ?');
   $stmt->execute(array($userId));
   $Polls = $stmt->fetchAll();
-
-  
-  
+ 
   foreach ($Polls as $tempPoll)
     $result[] = $tempPoll['Question'];    
 }
+
 else if(isset($_GET['id'])){
   $id = $_GET['id'];
    $stmt = $db->prepare('SELECT Question FROM UserQuery WHERE idUserQuery = ?');
   $stmt->execute(array($id));
   $Poll = $stmt->fetchAll();
 
+   $result[] = $Poll['Question'];
 }
+
 else if(isset($_GET['word'])){
 
-
+  $word = $_GET['word']);
   $stmt = $db->prepare('SELECT Question FROM UserQuery');
   $stmt->execute(array($userId));
   $Polls = $stmt->fetchAll();
   foreach ($Polls as $tempPoll)
-    $result[] = $tempPoll['Question'];  
+    if(strpos($word,$tempPoll['Question']))
+      $result[] = $tempPoll['Question'];  
 }
   echo json_encode($result);
 ?>
