92f0abc879e65cf0fcb29227921c595249d7cc7f
Psouka <p4so@gmail.com>
2014-11-27 22:17:15 +0000

    logout melhorado ^2

--- LTW/db/logout.php
+++ LTW/db/logout.php
@@ -5,16 +5,9 @@ session_start();
 	$username = $_SESSION['usernameOn'];
 	$ip = $_SESSION['ipOut'];
 
-	$check = 0;
-	$stmt = $dbh->prepare('SELECT idUser FROM User WHERE username = ?');
-	$stmt->execute(array($username));
-	$row = $stmt->fetch();
 
-	$id = $row['idUser'];
-
-
-	$stmt1 = $dbh->prepare('DELETE FROM UserLogin WHERE idUser = ? and IPUser = ?');
- 	$stmt1->execute(array($id,$ip));
+	$stmt1 = $dbh->prepare('DELETE FROM UserLogin WHERE IPUser = ?');
+ 	$stmt1->execute(array($ip));
 
 
 header( 'Location: ../html/page.php' );
