74a6919d50102587265d296655572bb618c10286
Psouka <p4so@gmail.com>
2014-11-29 20:21:12 +0000

    search fix2

--- LTW/html/createPoll.php
+++ LTW/html/createPoll.php
@@ -125,7 +125,7 @@
                          <script src="../js/searchPoll.js" language="Javascript" type="text/javascript"></script>
                     </div>
                     <input type="button" value="Search" class="button"
-                    onClick="searchUser(this.form.userInput.value);searchId(this.form.idInput.value);searchWord(this.form.wordInput.value);">
+                    onClick="searchPoll(this.form.userInput.value,this.form.idInput.value,this.form.wordInput.value);">
                 </p>
                     </ul>
                 </form>
--- LTW/js/searchPoll.js
+++ LTW/js/searchPoll.js
@@ -1,35 +1,59 @@
 
+
+function searchPoll(username,id,word)
+{
+	clearSearch();
+	searchUser(username);
+	searchId(id);
+	searchWord(word);
+}
+
 function searchUser(username) {
-if(username == "")
-	return;
-  $.getJSON("../db/searchPoll.php", {'username': username}, PollsReceived);
+	if(username == "")
+		return;
+	$.getJSON("../db/searchPoll.php", {'username': username}, PollsReceived);
 }
 
 function searchId(id) {
 	if(id == "")
-	return;
-  $.getJSON("../db/searchPoll.php", {'id': id}, PollsReceived);
+		return;
+	$.getJSON("../db/searchPoll.php", {'id': id}, PollsReceived);
 }
 
 function searchWord(word) {
 	if(word == "")
-	return;
-  $.getJSON("../db/searchPoll.php", {'word': word}, PollsReceived);
+		return;
+	$.getJSON("../db/searchPoll.php", {'word': word}, PollsReceived);
 }
 
 
 // Called when messages are received
 function  PollsReceived(data) {
-  $.each(data, resultPoll);
+	$.each(data, resultPoll);
 }
 
 // Called for each line received
 function resultPoll(index, value) {
 	
-var  a = document.createElement('a');
-var linkText = document.createTextNode(value);
-a.appendChild(linkText);
-a.title = value;
-a.href = "../db/poll.php?Question=" + value;
-document.getElementById('dynamicSearch').appendChild(a);
+	var  a = document.createElement('a');
+	var linkText = document.createTextNode(value);
+	a.appendChild(linkText);
+	a.title = value;
+	a.href = "../db/poll.php?Question=" + value;
+	document.getElementById('dynamicSearch').appendChild(a);
+
+	var br = document.createElement('br');
+	document.getElementById('dynamicSearch').appendChild(br);
+	document.getElementById('dynamicSearch').appendChild(br);
+
+}
+
+
+function clearSearch(){
+	var list = document.getElementById('dynamicSearch');
+
+	while (list.firstChild) {
+		list.removeChild(list.firstChild);
+	}
+
 }
