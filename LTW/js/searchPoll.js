554b1e34d4df67d1ac61e806741c6279ac31e3bc
Vítor Teixeira <vitor_filipe_@hotmail.com>
2014-11-29 21:48:13 +0000

    search css done

--- LTW/js/searchPoll.js
+++ LTW/js/searchPoll.js
@@ -1,6 +1,13 @@
 function searchPoll(username,id,word)
 {
 	clearSearch();
+	var elem = document.getElementById('dynamicSearch');
+	var h1 = document.createElement('H1');
+	var tE = document.createTextNode('Results: ');
+	h1.appendChild(tE);
+	elem.appendChild(h1);
+	elem.appendChild(document.createElement('br'));
+	elem.appendChild(document.createElement('br'));
 	searchUser(username);
 	searchId(id);
 	searchWord(word);
