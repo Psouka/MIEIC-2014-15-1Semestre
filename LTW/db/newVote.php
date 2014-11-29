4414e0a5833fd4a3035083ef870a7533c8cb7d30
Psouka <p4so@gmail.com>
2014-11-29 18:36:38 +0000

    asd

--- LTW/db/newVote.php
+++ LTW/db/newVote.php
@@ -2,45 +2,31 @@
 session_start(); 
 	$dbh = new PDO('sqlite:database.db');
 
-	$Query = $_SESSION['Query'];
-	$Option= $_SESSION['OptionX'];
-	$IpUser = $_SESSION['IpUser'];
-	$username = $_SESSION['Username'];
+	$idQuery = $_POST['idQuery'];
+	$OptionX= $_POST['OptionX'];
+	$IpUser = $_POST['IpUser'];
+	$username = $_POST['Username'];
 
 if($username != 'test')
 {
-
+	$idUser;
 	$stmt = $dbh->prepare('SELECT idUser FROM User WHERE username = ?');
 	$stmt->execute(array($username));
 	$row = $stmt->fetch();
-
+	$idUser = $row['idUser'];
 
 	$stmt = $dbh->prepare('INSERT INTO UserQuery (idUser,Question) VALUES (?, ?)');
 	$stmt->execute(array($row['idUser'], $quest));
-
 
 
 	$stmt = $dbh->prepare('SELECT idUserQuery FROM UserQuery  WHERE Question = ?');
 	$stmt->execute(array($quest));
 	$row = $stmt->fetch();
 
-	$stmt = $dbh->prepare('INSERT INTO Answer (idUserQuery,Answerino) VALUES (?, ?)');
-	$stmt->execute(array($row['idUserQuery'], $option1));
-
-	$stmt = $dbh->prepare('INSERT INTO Answer (idUserQuery,Answerino) VALUES (?, ?)');
-	$stmt->execute(array($row['idUserQuery'], $option2));
-	
-	$stmt = $dbh->prepare('INSERT INTO Answer (idUserQuery,Answerino) VALUES (?, ?)');
-	$stmt->execute(array($row['idUserQuery'], $option3));
-
-	$stmt = $dbh->prepare('INSERT INTO Answer (idUserQuery,Answerino) VALUES (?, ?)');
-	$stmt->execute(array($row['idUserQuery'], $option4));
-
 
 }
 else
 	echo('U WUT MATE');
 
-header( 'Location: ../html/createPoll.php' );
-exit();
+
 ?>
--- LTW/js/auxPoll.js
+++ LTW/js/auxPoll.js
@@ -1,8 +1,28 @@
+var seeOptions = 0;
 
-function showOptions(idQuery) {
-	$.getJSON("../db/getAnswers.php", {'idQuery' : idQuery}, OptionsReceived);
+
+function showOptions(idQuery,username,ip) {
+	if(seeOptions == 0)
+		$.getJSON("../db/getAnswers.php", {'idQuery' : idQuery}, OptionsReceived);
+	else
+		sendVote(idQuery,username,ip)
 }
 
+function sendVote(idQuery,username,ip){
+	var rates =  document.getElementsByName('Vote');
+	var OptionX= '';
+	for(var i = 0; i < rates.length; i++){
+		if(rates[i].checked){
+			OptionX = rates[i].value;
+		}
+	}
+	if(OptionX == '')
+	{
+		alert('Select One');
+		return;
+	}
+	$.postJSON("../db/newVote.php", {'idQuery' : idQuery, 'OptionX' : OptionX, 'IpUser' : ip , 'Username' : username}, alert('Registed'));
+}
 
 function  OptionsReceived(data) {
 	alert(data);
@@ -11,7 +31,7 @@ function  OptionsReceived(data) {
 
 // Called for each line received
 function resultOptions(index, value) {
-	
+	seeOptions = 1;
 	var radioHtml = value +'<input type="radio" name="Vote"';
 	radioHtml += 'value="' + value + '"'
 	radioHtml += '/>';
