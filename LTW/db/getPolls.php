8f1c8ba493d4c5c0aa3702f674479b1d6f8059d7
Psouka <p4so@gmail.com>
2014-12-05 21:54:10 +0000

    getPolls working

--- LTW/db/getPolls.php
+++ LTW/db/getPolls.php
@@ -8,7 +8,7 @@ if(isset($_GET['numPag'])){
 	$numP = $_GET['numPag'];
 }
 else {
-	$numP= 1;
+	$numP= 0;
 }
 
 $var = $numP * 10;
@@ -17,6 +17,9 @@ $limit = $var + 10;
 $stmt = $db->prepare("SELECT Question FROM UserQuery WHERE Privacy = ? and rowid >= '$var' and rowid < '$limit' ");
 $stmt->execute(array(0));
 $Temperino = $stmt->fetchAll();
+
+foreach ($Temperino as $temp)
+		$array[] = $temp['Question'];
 
 echo json_encode($array);
 // Retorna numa array as perguntas não privadas
