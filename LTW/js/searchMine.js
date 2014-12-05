3ecaa7f098e6a3d5ab6b9f0da6b1e8f999b4fc6c
Psouka <p4so@gmail.com>
2014-12-05 17:27:20 +0000

    js

--- LTW/js/searchMine.js
+++ LTW/js/searchMine.js
@@ -3,8 +3,11 @@ function searchPoll(username,id,word)
 	var elem = document.getElementById('dynamicSearch');
 	var h1 = document.createElement('H1');
 	var tE = document.createTextNode('My Polls: ');
+	var ul = document.createElement('ul');
+	ul.setAttribute('id', 'pollList');
 	h1.appendChild(tE);
 	elem.appendChild(h1);
+	elem.appendChild(ul);
 	elem.appendChild(document.createElement('br'));
 	elem.appendChild(document.createElement('br'));
 	searchUser(username);
@@ -24,13 +27,14 @@ function  PollsReceived(data) {
 
 // Called for each line received
 function resultPoll(index, value) {
-	
-	var elem = document.getElementById('dynamicSearch');
+	var elem = document.getElementById('pollList');
+	var li = document.createElement('li');
 	var a = document.createElement('a');
 	var linkText = document.createTextNode(value);
 	a.appendChild(linkText);
 	a.title = value;
 	a.href = "../db/poll.php?Question=" + value;
-	elem.appendChild(a);
+	li.appendChild(a);
+	elem.appendChild(li);
 	elem.appendChild(document.createElement('br'));
 }
